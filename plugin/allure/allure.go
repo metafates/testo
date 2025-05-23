de5e91a8dd8330d1f9818e8c1be6797b3ffa6e95
metafates <vvz.dev@gmail.com>
2025-05-23 11:09:34 +0300

    add allure

--- plugin/allure/allure.go
+++ plugin/allure/allure.go
@@ -1,33 +1,111 @@
 package allure
 
 import (
+	"cmp"
+	"fmt"
+	"time"
+
 	"testman"
 )
 
 type Allure struct {
 	*testman.T
 
-	title string
+	start, stop time.Time
+
+	labels      []Label
+	links       []Link
+	description string
+	status      Status
+
+	children []*Allure
 }
 
-func (*Allure) New(t *testman.T) *Allure { return &Allure{T: t} }
+func (a *Allure) New(t *testman.T) *Allure {
+	child := &Allure{T: t}
 
-func (a *Allure) Title(title string) {
-	a.title = title
-}
+	if a != nil {
+		a.children = append(a.children, child)
+	}
 
-func (a *Allure) AfterAll() {
-	a.Log("Allure.AfterAll")
-}
-
-func (a *Allure) BeforeAll() {
-	a.Log("Allure.BeforeAll")
+	return child
 }
 
 func (a *Allure) BeforeEach() {
-	a.Log("Allure.BeforeEach " + a.Name())
+	a.start = time.Now()
 }
 
 func (a *Allure) AfterEach() {
-	a.Log("Allure.AfterEach " + a.Name())
+	a.stop = time.Now()
 }
+
+func (a *Allure) AfterAll() {
+	fmt.Println("took", a.stop.Sub(a.start))
+
+	for _, child := range a.children {
+		fmt.Println("child", child.Name(), child.getStatus())
+	}
+}
+
+func (a *Allure) Description(desc string) {
+	a.description = desc
+}
+
+func (a *Allure) Links(links ...Link) {
+	a.links = append(a.links, links...)
+}
+
+func (a *Allure) Status(status Status) {
+	a.status = status
+}
+
+func (a *Allure) Labels(labels ...Label) {
+	a.labels = append(a.labels, labels...)
+}
+
+func (a *Allure) getStatus() Status {
+	if a.Skipped() {
+		return StatusSkipped
+	}
+
+	if a.Failed() {
+		return StatusFailed
+	}
+
+	return cmp.Or(a.status, StatusPassed)
+}
+
+type Label struct {
+	Name  string
+	Value string
+}
+
+type Link struct {
+	Name string
+	URL  string
+	Type string
+}
+
+type Parameter struct {
+	Name  string
+	Value string
+	Mode  ParameterMode
+}
+
+type ParameterMode string
+
+const (
+	ParameterModeDefault ParameterMode = "default"
+	ParameterModeMasked  ParameterMode = "masked"
+	ParameterModeHidden  ParameterMode = "hidden"
+)
+
+type Status string
+
+const (
+	StatusFailed  Status = "failed"
+	StatusBroken  Status = "broken"
+	StatusPassed  Status = "passed"
+	StatusSkipped Status = "skipped"
+	StatusUnknown Status = "unknown"
+)
